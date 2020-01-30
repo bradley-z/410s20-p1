02458befdb1808c546e6e404e1d0d866f090e332
Bradley Zhou <bradleyz@andrew.cmu.edu>
2020-01-30 02:27:21 -0500

    reworded a few instructions

--- kern/sokoban_game.c
+++ kern/sokoban_game.c
@@ -66,9 +66,9 @@ const char *level_complete_messages[] = {
 const char *instructions[] = {
     "0. You are represented by '@', boxes by 'o', and target locations by 'x'",
     "1. Use WASD to move the player onto empty squares",
-    "2. There is an equal number of boxes and storage locations",
+    "2. There is an equal number of boxes and target locations",
     "3. Boxes cannot be pulled, or pushed into other boxes or walls",
-    "4. Push all boxes into a storage location to complete the level",
+    "4. Push teach box into its own target location complete the level",
 };
 
 game_t current_game;
